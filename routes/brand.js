355dab4663c79e942907ff441e25ba2bc76f982f
Harsh Singh <sujeetsingh734321@gmail.com>
2023-04-22 00:12:23 +0530

    chnages

--- routes/brand.js
+++ routes/brand.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const BrandSchema = require('./../models/brand_schema');
+
+router.get('/', async (req, res) => {
+    try {
+        const brand = await BrandSchema.find();
+        res.status(200).json(brand);
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+});
+
+router.post('/add', async (req, res) => {
+    const brand = new BrandSchema({
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+    });
+    try {
+        const savedBrand = await brand.save();
+        res.status(200).json({ message: "Brand added successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error });
+    }
+});
 
 module.exports = router;

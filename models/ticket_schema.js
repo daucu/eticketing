bf57e9add8ffba5397f92a9cdd08d2ac3301edbd
Harsh Singh <sujeetsingh734321@gmail.com>
2023-04-24 23:33:32 +0530

    chnages

--- models/ticket_schema.js
+++ models/ticket_schema.js
@@ -1,53 +1,56 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-const ticketSchema = new mongoose.Schema({
-    brand: {
-        type: String,
-        required: true
-    },
-    requester: {
-        type: String,
-        required: true
-    },
-    assignee: {
-        type: String,
-        required: true
-    },
-    form: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true
+const ticketSchema = new mongoose.Schema(
+  {
+    ticket_no: {
+      type: Number,
+      required: true,
+      unique: true,
     },
     priority: {
-        type: String,
-        required: true
-    },
-    type_of_issue: {
-        type: String,
-        required: true
-    },
-    impact: {
-        type: String,
+      type: String,
+      required: true,
     },
     subject: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: String,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+    },
+    requester: {
+      type: String,
+    },
+    assignee: {
+      type: String,
+    },
+    form: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    type_of_issue: {
+      type: String,
+    },
+    impact: {
+      type: String,
     },
     status: {
-        type: String,
-        enum: ['active', 'resolved', 'unassigned'],
-        default: 'active'
+      type: String,
+      enum: ["active", "resolved", "unassigned"],
+      default: "active",
     },
-
-}, { timestamps: true });
-module.exports = mongoose.model('tickets', ticketSchema);
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("tickets", ticketSchema);
--- routes/ticket.js
+++ routes/ticket.js
@@ -1,89 +1,112 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const create_ticket = require('./../models/ticket_schema');
-const path = require('path');
+const create_ticket = require("./../models/ticket_schema");
+const path = require("path");
 const CheckAuth = require("./../functions/check_auth");
 
 //Create ticket route
 router.post("/", async (req, res) => {
+  //Check file upload
+  if (!req.files) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
 
-    //Check file upload
-    if (!req.files) {
-        return res.status(400).json({ message: "No file uploaded" });
-    }
+  const check = await CheckAuth(req, res);
 
-    const check = await CheckAuth(req, res);
+  if (check.auth === false) {
+    return res.status(401).json({ message: "Unauthorized", auth: false });
+  }
 
-    if (check.auth === false) {
-        return res.status(401).json({ message: "Unauthorized", auth: false });
-    }
+  let image_link1 = req.files.image_link;
+  //Remove old name and give new string name to file
+  image_link1.name = Date.now() + path.extname(image_link1.name);
+  image_link1.mv("./uploads/" + image_link1.name);
 
-    let image_link1 = req.files.image_link;
-    //Remove old name and give new string name to file
-    image_link1.name = Date.now() + path.extname(image_link1.name);
-    image_link1.mv('./uploads/' + image_link1.name);
+  //Get ticket fields
+  const ticket = new create_ticket({
+    ticket_no: req.body.ticket_no,
+    priority: req.body.priority,
+    subject: req.body.subject,
+    description: req.body.description,
+    requester: check.data._id,
+    brand: "no_data",
+    assignee: "no_data",
+    form: "no_data",
+    type: "no_data",
+    type_of_issue: "no_data",
+    impact: "no_data",
+    image: image_link1.name,
+    status: "active",
+  });
 
-    //Get ticket fields
-    const { brand, assignee, form, type, priority, type_of_issue, impact, subject, description } = req.body;
-    const ticket = new create_ticket({
-        brand: brand,
-        requester: check.data._id,
-        assignee: assignee,
-        form: form,
-        type: type,
-        priority: priority,
-        type_of_issue: type_of_issue,
-        impact: impact,
-        subject: subject,
-        image: image_link1.name,
-        description: description,
-        status: "active"
-    });
-
-    //Save ticket
-    await ticket.save();
-    res.status(200).json({
-        message: "Ticket created successfully"
-    });
+  //Save ticket
+  await ticket.save();
+  res.status(200).json({
+    message: "Ticket created successfully",
+  });
 });
 
 //Get my tickets
 router.get("/mytickets", async (req, res) => {
+  const check = await CheckAuth(req, res);
 
-    const check = await CheckAuth(req, res);
+  if (check.auth === false) {
+    return res.status(401).json({ message: "Unauthorized", auth: false });
+  }
 
-    if (check.auth === false) {
-        return res.status(401).json({ message: "Unauthorized", auth: false });
+  try {
+    const tickets = await create_ticket.find({ requester: check.data._id });
+    res.status(200).json(tickets);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "something went wrong" });
+  }
+});
+
+//Get ticket by Id
+router.get("/:ticketId", async (req, res) => {
+  const check = await CheckAuth(req, res);
+
+  if (check.auth === false) {
+    return res.status(401).json({ message: "Unauthorized", auth: false });
+  }
+
+  try {
+    const ticket = await create_ticket.findById(req.params.ticketId);
+    if (ticket.requester == check.data._id) {
+      res.status(200).json(ticket);
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
     }
-
-    try {
-        const tickets = await create_ticket.find({ requester: check.data._id });
-        res.status(200).json(tickets);
-    } catch (error) {
-        res.status(500).json({ error: error.message, message: "something went wrong" });
-    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "something went wrong" });
+  }
 });
 
 //Delete ticket
 router.delete("/:ticketId", async (req, res) => {
+  const check = await CheckAuth(req, res);
 
-    const check = await CheckAuth(req, res);
+  if (check.auth === false) {
+    return res.status(401).json({ message: "Unauthorized", auth: false });
+  }
 
-    if (check.auth === false) {
-        return res.status(401).json({ message: "Unauthorized", auth: false });
+  try {
+    const ticket = await create_ticket.findById(req.params.ticketId);
+    if (ticket.requester == check.data._id) {
+      await create_ticket.deleteOne({ _id: req.params.ticketId });
+      res.status(200).json({ message: "Ticket deleted successfully" });
+    } else {
+      res.status(401).json({ message: "Unauthorized" });
     }
-
-    try {
-        const ticket = await create_ticket.findById(req.params.ticketId);
-        if (ticket.requester == check.data._id) {
-            await create_ticket.deleteOne({ _id: req.params.ticketId });
-            res.status(200).json({ message: "Ticket deleted successfully" });
-        } else {
-            res.status(401).json({ message: "Unauthorized" });
-        }
-    } catch (error) {
-        res.status(500).json({ error: error.message, message: "something went wrong" });
-    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: error.message, message: "something went wrong" });
+  }
 });
 
 module.exports = router;
